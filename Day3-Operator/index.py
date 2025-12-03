93d577c327f3b69462de058f7de546ba621c57a5
Abhishek gupta <abhishekspps825406@gmail.com>
2025-12-03 09:10:16 +0530

    Day 3 done

---
+++ Day3-Operator/index.py
@@ -0,0 +1,38 @@
+
+print('Addition: ', 1 + 2)        # 3
+print('Subtraction: ', 2 - 1)     # 1
+print('Multiplication: ', 2 * 3)  # 6
+print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+print('Division: ', 6 / 2)        # 3.0         
+print('Division: ', 7 / 2)        # 3.5
+print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+print ('Division without the remainder: ',7 // 3)   # 2
+print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+
+
+# Calculating (area,volume,density,weight,perimeter,distance,force)
+
+# Calculating area of a circle
+radius = 10                                 # radius of a circle
+area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')                         # Adding unit to the weight
+
+# Calculate the density of a liquid
+mass = 75 # in Kg
+volume = 0.075 # in cubic meter
+density = mass / volume # 1000 Kg/m^3
+
+
