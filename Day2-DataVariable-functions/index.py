68d0995a9ee25e69e33902fd6df4fbec28673ec3
Abhishek gupta <abhishekspps825406@gmail.com>
2025-12-03 09:07:18 +0530

    Day 2 completed

---
+++ Day2-DataVariable-functions/index.py
@@ -0,0 +1,9 @@
+# Multiple variables in a line
+first_name , last_name , age , is_Married , country = "India","No",23,"Gupta","Abhishek"
+
+print(first_name,last_name,age,is_Married,country)
+
+# Getting user input
+
+age1 = input("Enter ur age")
+print("My age is  = " ,age1)
