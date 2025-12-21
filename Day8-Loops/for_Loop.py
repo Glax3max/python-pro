ab85b2b58ad1dbef1d1ac5716b003edc5c42fcf7
Abhishek gupta <abhishekspps825406@gmail.com>
2025-12-21 18:34:28 +0530

    Loops done

---
+++ Day8-Loops/for_Loop.py
@@ -0,0 +1,24 @@
+lst = ['A','B','C','D','E','F','G']
+
+for iterator in lst:
+    print(iterator)
+    
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+
+for key in person:
+    print(key)
+
+
+for key, value in person.items():
+    print(key, ' - ', value)
