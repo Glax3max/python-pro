31efd4fafdfe7dffa48c4d4f5ba2cac8a6973c3e
Abhishek gupta <abhishekspps825406@gmail.com>
2025-12-21 18:26:51 +0530

    Conditional done

---
+++ Day7-ConditionChecking/conditional.py
@@ -0,0 +1,13 @@
+
+
+if 3>4:
+    print("Greater")
+else:
+    print("not greater")
+
+user = 'James'
+access_level = 3
+if user == 'admin' or access_level >= 4:
+        print('Access granted!')
+else:
+    print('Access denied!')
